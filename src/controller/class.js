e20d55095f799d8f007b11196c95d988c82581a1
KungTran <trancongtr1071997@gmail.com>
2022-08-16 01:14:57 +0700

    add

--- src/controller/class.js
+++ src/controller/class.js
@@ -4,7 +4,6 @@ const { RESPONSE_CODE } = require("../constant");
 const getAllClassController = async (req, res) => {
     const [rows, fields] = await db.promise().query("SELECT * FROM class");
 
-
     res.send({
         code: RESPONSE_CODE.SUCCESS,
         message: "Get all class successful",
