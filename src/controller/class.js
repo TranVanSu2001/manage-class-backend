849607256cd0c5af8589304326b075f1ee10d753
KungTran <trancongtr1071997@gmail.com>
2022-08-16 01:13:18 +0700

    add

--- src/controller/class.js
+++ src/controller/class.js
@@ -1,10 +1,12 @@
 const db = require("../service/db");
+const { RESPONSE_CODE } = require("../constant");
 
 const getAllClassController = async (req, res) => {
     const [rows, fields] = await db.promise().query("SELECT * FROM class");
 
 
     res.send({
+        code: RESPONSE_CODE.SUCCESS,
         message: "Get all class successful",
         data: rows
     });
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/", router);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port: ${PORT}`);
 });
 
 
